b7d9521b2be3024ebd74bdc7dbc547c1517be257
Carlos Tovar <jctovarmunoz@gmail.com>
2017-07-17 16:27:36 -0500

    Conversion de Decimal a Binario, Hexadecimal y Octal

---
+++ Tarea1ConversionBase.c
@@ -0,0 +1,32 @@
+/*
+TAREA 1 CONVERSION DE DECIMAL A BINARIO, DECIMAL Y HEXADECIMAL
+TOVAR MUNOZ JOSE CARLOS
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    short x, b;
+    int i;
+    printf("x=");
+    scanf("%hd",&x);
+    printf("\n DECIMAL: %d",x);
+    printf("\n HEXADECIMAL:");
+    for(i=3; i<16; i=i+4){
+        b=x>>(15-i);
+        b=b&15;
+        printf("%x",b);//Hexadecimal
+    }
+    
+	printf("\n BINARIO:");
+    for(i=0; i<16; i++){
+        b=x>>(15-i);
+        b=b&1;
+        printf("%d",b);//Hexadecimal
+    }
+    printf("\n OCTAL: %o",x); //OCTAL
+
+    return 0;
+}
+
